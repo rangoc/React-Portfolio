07831b36bb8a7cfb7629d13e2fabe0a18233cf50
goranc <cabarkapa.goran@gmail.com>
2021-04-21 16:59:05 +0200

    Styling intro section

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,9 +1,26 @@
 import './intro.scss';
-
+import man from 'assets/man.png';
+import downArrow from 'assets/down.png';
 const Intro = () => {
   return (
     <div className="intro" id="intro">
-      intro
+      <div className="left">
+        <div className="imgContainer">
+          <img src={man} alt="man" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I am</h2>
+          <h1>Dev Kappa</h1>
+          <h3>
+            Freelance <span>developer</span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src={downArrow} alt="down arrow" />
+        </a>
+      </div>
     </div>
   );
 };
